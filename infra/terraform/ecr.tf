faef3de2b028c62a8ca69f1d170c3f253c812cf7
Fernando Manuel <fbenitesm2@upao.edu.pe>
2026-06-26 23:40:55 -0500

    fix(terraform): enable KMS encryption for ECR (CKV_AWS_136)

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -25,6 +25,10 @@ resource "aws_ecr_repository" "ticketgo_api" {
     scan_on_push = true
   }
 
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
   tags = {
     Name = "ticketgo-api"
   }

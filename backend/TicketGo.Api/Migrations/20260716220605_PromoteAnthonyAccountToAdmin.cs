f0b21fff7605f502772c75f97331ef79870b12ee
Anthony Gaytan <gaytan150518@gmail.com>
2026-07-16 17:07:01 -0500

    feat(auth): promote production account to admin

---
+++ backend/TicketGo.Api/Migrations/20260716220605_PromoteAnthonyAccountToAdmin.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketGo.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PromoteAnthonyAccountToAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Users\" SET \"Role\" = 'Admin' WHERE LOWER(\"Email\") = LOWER('Gaytan150518@gmail.com');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Users\" SET \"Role\" = 'Customer' WHERE LOWER(\"Email\") = LOWER('Gaytan150518@gmail.com');");
+        }
+    }
+}
